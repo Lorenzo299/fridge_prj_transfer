8c0668674e55565047b57944a8ef325eef365782
Lorenzo299 <lorenzo2gatti9@gmail.com>
2025-10-17 10:54:01 +0200

    modifiche pc aziendale

--- src/main/java/com/gdserv/fridge/models/Product.java
+++ src/main/java/com/gdserv/fridge/models/Product.java
@@ -15,8 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
+
 
 @Entity
 @Table(name = "products")
